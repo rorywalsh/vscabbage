aaae0086fb840eefd12c843165e35989d1595106
rorywalsh <rorywalsh@ear.ie>
2024-11-25 09:15:52 +0000

    updating copy script

--- copySrc.sh
+++ copySrc.sh
@@ -1,2 +1,16 @@
-cp -rfv ./src/* /Users/rwalsh/Library/CabbageAudio/CabbagePluginEffect/
-cp -rfv ./media/cabbage.css /Users/rwalsh/Library/CabbageAudio/CabbagePluginEffect/
+#!/bin/bash
+
+# Set the source directories
+SRC_DIR="./src/*"
+CSS_FILE="./media/cabbage.css"
+
+# Check if running on Windows (CYGWIN, MSYS, or WSL) or macOS/Linux
+if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* || "$OSTYPE" == "win32" || "$OSTYPE" == "win64" ]]; then
+    DEST_DIR="/c/ProgramData/CabbageAudio/CabbagePluginEffect"
+else
+    DEST_DIR="/Users/rwalsh/Library/CabbageAudio/CabbagePluginEffect"
+fi
+
+# Copy files from the source directory to the destination
+cp -rfv $SRC_DIR "$DEST_DIR"
+cp -rfv $CSS_FILE "$DEST_DIR"
