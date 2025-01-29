65bf92416b673d287686dbf4cbf372d5c83b8629
rorywalsh <rorywalsh@ear.ie>
2025-01-29 16:02:02 +0000

    updating copy script for Linux

--- copySrc.sh
+++ copySrc.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
-# Define the target directory
+# Define target directories based on the OS
 if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* || "$OSTYPE" == "win32" || "$OSTYPE" == "win64" ]]; then
     TARGET_DIR_FX="/c/ProgramData/CabbageAudio/CabbageVST3Effect/cabbage/"
     TARGET_CSS_DIR_FX="/c/ProgramData/CabbageAudio/CabbageVST3Effect/"
     TARGET_DIR="/c/ProgramData/CabbageAudio/CabbageVST3Synth/cabbage/"
     TARGET_CSS_DIR="/c/ProgramData/CabbageAudio/CabbageVST3Synth/"
-else
-    TARGET_DIR_FX="/Users/rwalsh/Library/CabbageAudio/CabbageVST3Effect/cabbage/"
-    TARGET_CSS_DIR_FX="/Users/rwalsh/Library/CabbageAudio/CabbageVST3Effect/"
-    TARGET_DIR="/Users/rwalsh/Library/CabbageAudio/CabbageVST3Synth/cabbage/"
-    TARGET_CSS_DIR="/Users/rwalsh/Library/CabbageAudio/CabbageVST3Synth/"
+elif [[ "$OSTYPE" == "darwin"* ]]; then  # macOS
+    TARGET_DIR_FX="$HOME/Library/CabbageAudio/CabbageVST3Effect/cabbage/"
+    TARGET_CSS_DIR_FX="$HOME/Library/CabbageAudio/CabbageVST3Effect/"
+    TARGET_DIR="$HOME/Library/CabbageAudio/CabbageVST3Synth/cabbage/"
+    TARGET_CSS_DIR="$HOME/Library/CabbageAudio/CabbageVST3Synth/"
+else  # Linux
+    TARGET_DIR_FX="$HOME/.config/CabbageAudio/CabbageVST3Effect/cabbage/"
+    TARGET_CSS_DIR_FX="$HOME/.config/CabbageAudio/CabbageVST3Effect/"
+    TARGET_DIR="$HOME/.config/CabbageAudio/CabbageVST3Synth/cabbage/"
+    TARGET_CSS_DIR="$HOME/.config/CabbageAudio/CabbageVST3Synth/"
 fi
 
-# Check if the target directory exists, and create it if it doesn't
-if [ ! -d "$TARGET_DIR" ]; then
-    echo "Target directory $TARGET_DIR does not exist. Creating it."
-    mkdir -p "$TARGET_DIR"
-else
-    echo "Target directory $TARGET_DIR already exists."
-fi
+# Function to create directory if it doesn't exist
+create_dir_if_not_exists() {
+    if [ ! -d "$1" ]; then
+        echo "Creating directory: $1"
+        mkdir -p "$1"
+    else
+        echo "Directory already exists: $1"
+    fi
+}
+
+# Ensure the target directories exist
+create_dir_if_not_exists "$TARGET_DIR"
+create_dir_if_not_exists "$TARGET_DIR_FX"
+create_dir_if_not_exists "$TARGET_CSS_DIR"
+create_dir_if_not_exists "$TARGET_CSS_DIR_FX"
 
 # Copy the source files
 cp -rfv ./src/cabbage/* "$TARGET_DIR"
