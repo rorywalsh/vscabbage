99d1e86a4ccfc23a1845bb1a91821da11b461592
rorywalsh <rorywalsh@ear.ie>
2026-02-22 13:48:59 +0000

    Adding presistence objects to widgets

--- src/cabbage/widgets/infoButton.js
+++ src/cabbage/widgets/infoButton.js
@@ -28,11 +28,6 @@ export class InfoButton extends Button {
             "radioGroup": -1,
             "type": "infoButton",
 
-            "persistence": {
-                "preset": true,
-                "session": true
-            },
-
             "style": {
                 "opacity": 1,
                 "borderRadius": 6,
